c0a5a68915aff79ead027d3e0ba87fe24c42c31e
bill donner <billdonner@gmail.com>
2024-06-12 13:19:18 -0500

    Initial Commit

---
+++ basic/basicApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicApp.swift
+//  basic
+//
+//  Created by bill donner on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct basicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ basicTests/basicTests.swift
@@ -0,0 +1,17 @@
+//
+//  basicTests.swift
+//  basicTests
+//
+//  Created by bill donner on 6/12/24.
+//
+
+import Testing
+@testable import basic
+
+struct basicTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
